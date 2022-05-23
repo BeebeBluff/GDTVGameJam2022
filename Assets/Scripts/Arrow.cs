985ae7d48f615d73db1770f43b2f8453ea588f1c
Matt Beebe <mbb720@hotmail.com>
2022-05-23 16:28:02 -0400

    Added arrow force and arrows stick into their collided objects.

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     float arrowKillTime = 10f;
+    float arrowForce = 20f;
+
     Rigidbody myRigidBody;
+
+    private void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -14,6 +21,23 @@ public class Arrow : MonoBehaviour
         if (arrowKillTime <= 0f)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) { return; } //avoid collision with the player
+
+        Destroy(myRigidBody);
+        Destroy(GetComponent<BoxCollider>()); //stop motion and future collisions
+
+        gameObject.transform.SetParent(other.gameObject.transform);
+        
+        if(other.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            Rigidbody collisionRigidbody = other.gameObject.GetComponent<Rigidbody>();
+
+            collisionRigidbody.AddForce(transform.forward * arrowForce);
         }
     }
 
