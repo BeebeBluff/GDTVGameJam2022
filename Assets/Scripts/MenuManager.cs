d41175094470b0173df82e6f959dd92e98bf966f
Matt Beebe <mbb720@hotmail.com>
2022-05-29 12:48:10 -0400

    Added an instructions scene and made the cursor visible when menus are open.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+    void Update()
+    {
+
+    }
+
+    public void LoadNextScene()
+    {
+        Time.timeScale = 1;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadCreditsScene()
+    {
+        //Load Credits Scene or just canvas?
+    }
+
+}
